dd0fa242255932f1585c44556db3d0c0c55fea90
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2021-12-07 19:12:12 +0100

    Added break down by usage type of top 5 changes

--- aws-cost-and-usage-report.py
+++ aws-cost-and-usage-report.py
@@ -10,6 +10,112 @@ import os
 from calendar import monthrange
 from dateutil.relativedelta import relativedelta
 
+def get_cost_and_usage(start_date, end_date, group_by=[{'Type': 'DIMENSION', 'Key': 'SERVICE'}], granularity='MONTHLY', metrics=['UnblendedCost'], **kwargs):
+    results = []
+    token = None
+
+    session = boto3.session.Session()
+    ce = session.client('ce')
+    while True:
+        if token:
+            params = {'NextPageToken': token} + kwargs
+        else:
+            params = kwargs
+        data = ce.get_cost_and_usage(
+            TimePeriod={'Start': str(start_date), 'End':  str(end_date)},
+            Granularity=granularity,
+            Metrics=metrics,
+            GroupBy=group_by,
+            **params)
+        results += data['ResultsByTime']
+        token = data.get('NextPageToken')
+        if not token:
+            break
+
+    return results
+
+
+# Example result that we need to parse
+# [{'TimePeriod': {'Start': '2021-05-01', 'End': '2021-06-01'},
+#   'Total': {},
+#   'Groups': [
+#     {'Keys': ['AWS Backup'], 'Metrics': {'UnblendedCost': {'Amount': '0.6320089524', 'Unit': 'USD'}}},
+#     {'Keys': ['AWS CloudTrail'], 'Metrics': {'UnblendedCost': {'Amount': '36.566997', 'Unit': 'USD'}}},
+#     {'Keys': ['AWS Config'], 'Metrics': {'UnblendedCost': {'Amount': '238.304', 'Unit': 'USD'}}},
+#     {'Keys': ['AWS Database Migration Service'], 'Metrics': {'UnblendedCost': {'Amount': '26.676219876', 'Unit': 'USD'}}}
+# Above might change if query parameters are altered
+
+# The first thing we do is we get the name of all services for all month
+# We want to deal with the situation when we don't have a sevice for a first month but then it get added
+# during the second or the third month. Thus we want to pre-populate those services with 0 since we are using
+# append to form rows
+def ce_response_to_dataframe(input):
+    all_keys = []
+    rows = {}
+    column_names = []
+    for month in input:
+        for group in month['Groups']:
+            key_name = group['Keys'][0]
+            if key_name not in all_keys:
+                all_keys.append(key_name)
+    logging.debug(f'all_keys:\n{all_keys}')
+
+    # Now when we know all the keys we need to deal with, we can start collecting data
+    for month in input:
+        column_names.append(month['TimePeriod']['Start'])
+        all_keys_for_this_month = []
+        for group in month['Groups']:
+            key_name = group['Keys'][0]
+            all_keys_for_this_month.append(key_name)
+            if key_name not in rows:
+                rows[key_name] = []
+            rows[key_name].append(float(group['Metrics']['UnblendedCost']['Amount']))
+        # now we need to add all keys that wasn't mentioned in this month but exist in
+        # the final reports
+        # first check that there are such keys
+        if all_keys_for_this_month == all_keys:
+            continue
+        for key_name in all_keys:
+            if key_name not in all_keys_for_this_month:
+                if key_name not in rows:
+                    rows[key_name] = []
+                rows[key_name].append(float(0))
+
+    df = pandas.DataFrame(rows.values(), columns=column_names, index=all_keys)
+    df.fillna(value=0, inplace=True)
+    df = df.round(2)
+    logging.debug(f'Initial data frame:\n{df}\n')
+
+    # drop all the rows with zeros since there could be quite many
+    # after rounding
+    df = df.loc[(df.iloc[: , 1: ] != 0).all(1)]
+
+    # calculate and append total cost. Note important that we do it before sorting
+    row_with_total = []
+    for column in df.columns:
+        row_with_total.append(df[column].sum())
+    row_wiht_total_series = pandas.Series(row_with_total, index = df.columns, name='Total Cost')
+    df = df.append(row_wiht_total_series)
+
+    # Sort data frame
+    df.sort_values(df.columns.tolist(), ascending=False, inplace=True)
+
+    final_df = df.copy()
+    for column in df.columns:
+        # column = 2021-05-01
+        year = int(column.split('-')[0])
+        month = int(column.split('-')[1])
+        number_of_days = monthrange(year, month)[1]
+        final_df[f'n {column}'] = df[column].div(number_of_days).round(2)
+
+    # Insert separator between regular columns and normalized columns so it is easier
+    # to write to file later on
+    final_df.insert(3,'---','')
+
+    logging.debug(f'Data frame with normalized data:\n{final_df}\n')
+
+    return final_df
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                  description="Generate cost and usage report for the last 3 month grouped by service")
 # pass sensitivity
@@ -35,109 +141,42 @@ user_id = sts.get_caller_identity().get('Arn').split(':')[-1]
 logging.info(f'Getting montly cost and usage report from {start} to {end}')
 logging.info(f'Cost change sensitivity is set to {sensitivity}')
 
-session = boto3.session.Session()
-cd = session.client('ce')
-
-results = []
-
-token = None
-while True:
-    if token:
-        kwargs = {'NextPageToken': token}
-    else:
-        kwargs = {}
-    data = cd.get_cost_and_usage(
-        TimePeriod={'Start': str(start), 'End':  str(end)},
-        Granularity='MONTHLY',
-        Metrics=['UnblendedCost'],
-        GroupBy=[{'Type': 'DIMENSION', 'Key': 'SERVICE'}],
-        **kwargs)
-    results += data['ResultsByTime']
-    token = data.get('NextPageToken')
-    if not token:
-        break
+results = get_cost_and_usage(start, end)
 
 logging.debug(f'Response:\n{results}')
 
 logging.info('Parsing report')
 
-rows = {}
-column_names = ['Service']
-all_services = []
-# Example result that we need to parse
-# [{'TimePeriod': {'Start': '2021-05-01', 'End': '2021-06-01'},
-#   'Total': {},
-#   'Groups': [
-#     {'Keys': ['AWS Backup'], 'Metrics': {'UnblendedCost': {'Amount': '0.6320089524', 'Unit': 'USD'}}},
-#     {'Keys': ['AWS CloudTrail'], 'Metrics': {'UnblendedCost': {'Amount': '36.566997', 'Unit': 'USD'}}},
-#     {'Keys': ['AWS Config'], 'Metrics': {'UnblendedCost': {'Amount': '238.304', 'Unit': 'USD'}}},
-#     {'Keys': ['AWS Database Migration Service'], 'Metrics': {'UnblendedCost': {'Amount': '26.676219876', 'Unit': 'USD'}}}
-# Above might change if query parameters are altered
-
-# The first thing we do is we get the name of all services for all month
-# We want to deal with the situation when we don't have a sevice for a first month but then it get added
-# during the second or the third month. Thus we want to pre-populate those services with 0 since we are using
-# append to form rows
-for month in results:
-    for service in month['Groups']:
-        service_name = service['Keys'][0]
-        if service_name not in all_services:
-            all_services.append(service_name)
-logging.debug(f'all_services:\n{all_services}')
-
-# Now when we know all the service we need to deal with we can start collecting data
-for month in results:
-    column_names.append(month['TimePeriod']['Start'])
-    all_services_for_this_month = []
-    for service in month['Groups']:
-        service_name = service['Keys'][0]
-        all_services_for_this_month.append(service_name)
-        if service_name not in rows:
-            rows[service_name] = [service_name]
-        rows[service_name].append(float(service['Metrics']['UnblendedCost']['Amount']))
-    # now we need to add all services that wasn't mentioned in this month but exist in
-    # the final reports
-    # first check that there are such services
-    if all_services_for_this_month == all_services:
-        continue
-    for service_name in all_services:
-        if service_name not in all_services_for_this_month:
-            if service_name not in rows:
-                rows[service_name] = [service_name]
-            rows[service_name].append(float(0))
-
-df = pandas.DataFrame(rows.values(), columns=column_names)
-df.fillna(value=0, inplace=True)
-df = df.round(2)
+df = ce_response_to_dataframe(results)
 
 logging.debug(f'Results converted to data frame:\n{df}\n')
 
-logging.info('Calculating total cost per month')
+logging.info('Calculating services with most differences and getting usage type break down for the top 5')
+num_of_col = len(df.columns)
+last_month_norm_column_name = df.columns[num_of_col - 1]
+month_before_last_norm_column_name = df.columns[num_of_col - 2]
+# This one get diff between last column and one before last and then returns indexes of rows with the max diff
+rows_sorted_by_max_diff = (df[last_month_norm_column_name] - df[month_before_last_norm_column_name]).sort_values(ascending=False).index.values.tolist()
+# Have to remove 'Total cost' row otherwise it will be always in the top 5
+rows_sorted_by_max_diff.remove('Total Cost')
+top_five_services_by_max_diff = rows_sorted_by_max_diff[0:5]
 
-# calculate total cost per month
-row_with_total = ['Total cost']
-for column in column_names[1:]:
-    row_with_total.append(df[column].sum())
-
-df.loc[len(df.index)] = row_with_total
-
-logging.debug(f'Data frame with added totals:\n{df}\n')
-
-df = df.sort_values(column_names[-1], ascending=False)
-
-logging.debug(f'Sorted data frame with added totals:\n{df}\n')
-
-logging.info('Calculating normalized cost per month')
-
-normalized_df = df.copy()
-for column in column_names[1:]:
-    # column = 2021-05-01
-    year = int(column.split('-')[0])
-    month = int(column.split('-')[1])
-    number_of_days = monthrange(year, month)[1]
-    normalized_df[column] = normalized_df[column].div(number_of_days).round(2)
-
-logging.debug(f'Normalized by number of days in a month data frame:\n{normalized_df}\n')
+top_five_services_df = {}
+row_names = df.axes[0].tolist()
+for service_name in top_five_services_by_max_diff:
+    service_filter = {
+            "Dimensions": {
+                "Key": "SERVICE",
+                "Values": [service_name]
+            }
+        }
+    result = get_cost_and_usage(start, end, group_by=[{'Type': 'DIMENSION', 'Key': 'USAGE_TYPE', }], Filter=service_filter)
+    top_five_df = ce_response_to_dataframe(result)
+    top_five_df.sort_values(df.columns.tolist(), ascending=False, inplace=True)
+    top_five_services_df[service_name] = {
+        'df': top_five_df.copy(),
+        'diff': df.loc[service_name][last_month_norm_column_name] - df.loc[service_name][month_before_last_norm_column_name]
+        }
 
 logging.info(f'Writing repot to {report_file_name}')
 
@@ -148,28 +187,38 @@ worksheet_name = 'Cost and usage report'
 table_row_number = 6
 sensitivity_value_cell = '$B$3'
 normalized_cost_start_column_number = 5
-normalized_cost_start_column_letter = chr(ord('@') + normalized_cost_start_column_number + 1)
-normalized_cost_end_column_letter = chr(ord('@') + normalized_cost_start_column_number + len(column_names) - 1)
-comments_column_letter = chr(ord('@') + normalized_cost_start_column_number + len(column_names))
-suggestions_column_letter = chr(ord('@') + normalized_cost_start_column_number + len(column_names) + 1)
+normalized_cost_start_column_letter = chr(ord('@') + normalized_cost_start_column_number)
+normalized_cost_end_column_letter = chr(ord('@') + len(df.columns) + 1)
+comments_column_letter = chr(ord('@') + len(df.columns) + 2)
+suggestions_column_letter = chr(ord('@') + len(df.columns) + 3)
 # E0110: Abstract class 'ExcelWriter' with abstract methods instantiated (abstract-class-instantiated)
 # pylint: disable=E0110
 with pandas.ExcelWriter(report_file_name, engine='xlsxwriter') as writer:
+
     df.to_excel(writer,
                 sheet_name=worksheet_name,
                 startrow=table_row_number,
                 startcol=0,
-                index=False)
-    normalized_df.loc[:, df.columns != 'Service'].to_excel(writer,
-                                                           sheet_name=worksheet_name,
-                                                           startrow=table_row_number,
-                                                           startcol=normalized_cost_start_column_number,
-                                                           index=False)
+                index=True)
+
+    workbook = writer.book
+    worksheet = writer.sheets[worksheet_name]
+    row_counter = table_row_number + len(df.index.values.tolist()) + 3
+    worksheet.write('A' + str(row_counter), 'Top 5 services break down by usage type')
+    row_counter += 1
+
+    for service in top_five_services_by_max_diff:
+        worksheet.write('A' + str(row_counter), f'{service}\ndiff: {top_five_services_df[service]["diff"]}')
+        top_five_services_df[service]['df'].to_excel(writer,
+                                                sheet_name=worksheet_name,
+                                                startrow=row_counter,
+                                                startcol=0,
+                                                index=True)
+        row_counter = row_counter + len(top_five_services_df[service]['df'].index.values.tolist()) + 3
 
     # E1101: Instance of 'ExcelWriter' has no 'book' member (no-member)
     # pylint: disable=E1101
-    workbook = writer.book
-    worksheet = writer.sheets[worksheet_name]
+
     merged_cell_format = workbook.add_format()
     merged_cell_format.set_text_wrap(True)
     merged_cell_format.set_align('center')
@@ -180,7 +229,7 @@ with pandas.ExcelWriter(report_file_name, engine='xlsxwriter') as writer:
     # Set width and format of services columnt
     worksheet.set_column(0, 0, 23, text_column_format)
     # Set width of montly and daily cost columnts
-    worksheet.set_column(1, 7, 10)
+    worksheet.set_column(1, 7, 12)
     # Set width of column that separates montly cost from daily cost to 5
     # to leave more space for comments and suggestions
     worksheet.set_column(4, 4, 5)
